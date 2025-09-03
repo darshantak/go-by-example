fe3a33eb12aaa3044075838ac24f4bd03dfc4b8a
Darshan <darshanmohant@gmail.com>
2025-09-03 11:09:47 +0530

    connection pool

--- exercises/exercise1.go
+++ exercises/exercise1.go
@@ -1,5 +1,88 @@
 package exercises
 
-func Exercise1() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+type MockConnection struct {
+	id int
+}
+
+func NewMockConnection(id int) *MockConnection {
+	fmt.Printf("Connection %d created\n", id)
+	return &MockConnection{id: id}
+}
+
+func (c *MockConnection) Execute(query string) {
+	fmt.Printf("Connection %d executing query %s\n ", c.id, query)
+	time.Sleep(100 * time.Millisecond)
+}
+
+type ConnectionPool struct {
+	connections chan *MockConnection
+	size        int
+	mu          sync.Mutex
+}
+
+func NewConnectionPool(size int) *ConnectionPool {
+	pool := &ConnectionPool{
+		connections: make(chan *MockConnection, size),
+		size:        size,
+	}
+	for i := 0; i < size; i++ {
+		pool.connections <- NewMockConnection(i)
+	}
+	return pool
+}
+
+func (p *ConnectionPool) GetConnection() *MockConnection {
+	conn := <-p.connections
+	fmt.Printf("Connection %d acquired. Connections in pool: %d\n", conn.id, len(p.connections))
+	return conn
+}
+
+func (p *ConnectionPool) ReleaseConnection(conn *MockConnection) {
+	p.connections <- conn
+	fmt.Printf("Connection %d released. Connections in pool: %d\n", conn.id, len(p.connections))
+}
+
+func (p *ConnectionPool) CloseAll() {
+	close(p.connections)
+	for conn := range p.connections {
+		fmt.Printf("Connection %d closed\n", conn.id)
+	}
+	fmt.Println("Connection pool closed")
+}
+
+func worker(id int, pool *ConnectionPool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Worker %d trying to get a connection...\n", id)
+	conn := pool.GetConnection()
+	if conn != nil {
+		defer pool.ReleaseConnection(conn)
+		conn.Execute(fmt.Sprintf("SELECT * from users WHERE id=%d\n", id))
+	} else {
+		fmt.Printf("Worker %d could not get a connection\n", id)
+	}
+}
+
+func ConnectionPoolExercise() {
+	poolSize := 3
+	numTasks := 10
+	var wg sync.WaitGroup
+
+	connPool := NewConnectionPool(poolSize)
+	fmt.Printf("\nStarting %d tasks with a pool of size %d...\n\n", numTasks, poolSize)
+
+	for i := 0; i < numTasks; i++ {
+		wg.Add(1)
+		go worker(i, connPool, &wg)
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	wg.Wait()
+	fmt.Println("All tasks completed. Cleaning up pool")
 }
--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"fastline/exercises"
-	"fmt"
-	"os"
-)
-
-var exerciseMapper = map[string]func(){
-	"exercise1": exercises.Exercise1,
-}
+import "fastline/exercises"
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage : go run main.go <exerciseID>")
-		fmt.Println("Available exercises are :")
-		for k := range exerciseMapper {
-			fmt.Println(k)
-		}
-		return
-	}
-	fmt.Println("Checking in github")
-	exerciseId := os.Args[1]
-	if exercise, exists := exerciseMapper[exerciseId]; exists {
-		fmt.Printf("Running %s....", exerciseId)
-		exercise()
-	} else {
-		fmt.Println("Exercise not found")
-	}
-
+	exercises.ConnectionPoolExercise()
 }
